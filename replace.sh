e22e61a9e6a1b59b74536996d0e9661fa48a305c
bobbintb <bobbintb@gmail.com>
2025-06-10 17:22:36 -0600

    Create replace.sh

---
+++ replace.sh
@@ -0,0 +1,21 @@
+. /etc/unraid-version
+mkdir -p /boot/bzbackup
+mv /boot/bzimage /boot/bzbackup/bzimage.${version}
+mv /boot/bzimage.sha256 /boot/bzbackup/bzimage.${version}.sha256
+
+mv /boot/bzmodules /boot/bzbackup/bzmodules.${version}
+mv /boot/bzmodules.sha256 /boot/bzbackup/bzmodules.${version}.sha256
+
+wget -O /boot/bzimage https://github.com/bobbintb/unraid_ebpf/raw/refs/heads/main/${version}/bzimage-${version}
+wget -O /boot/bzimage.sha256 https://github.com/bobbintb/unraid_ebpf/raw/refs/heads/main/${version}/bzimage-${version}.sha256
+
+i=1
+while true; do
+  part=$(printf "%02d" "$i")
+  wget -q --spider "https://github.com/bobbintb/unraid_ebpf/raw/refs/heads/main/${version}/bzmodules-${version}.part.${part}" || break
+  wget -O /boot/bzmodules.part.${part} "https://github.com/bobbintb/unraid_ebpf/raw/refs/heads/main/${version}/bzmodules-${version}.part.${part}"
+  i=$((i+1))
+done
+
+cat bzmodules.part.* > bzmodules && rm bzmodules.part.*
+wget -O /boot/bzmodules.sha256 https://github.com/bobbintb/unraid_ebpf/raw/refs/heads/main/${version}/bzmodules-${version}.sha256
